17d3d58e703c75e1c11d4da2b8ff703d29b16571
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-06-24 14:21:05 -0700

    use scroll demo

---
+++ react-animations/src/components/Scrolling.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+import { useSpring, animated, config} from 'react-spring'
+
+function Scrolling() {
+  const [flip, set] = useState(false)
+
+  const words = ['We', 'came.', 'We', 'saw.', 'We', 'kicked', 'its', 'ass.']
+
+  const { scroll } = useSpring({
+    scroll: (words.length - 1) * 50,
+    from: { scroll: 0 },
+    reset: true,
+    reverse: flip,
+    delay: 200,
+    config: config.molasses,
+    onRest: () => set(!flip),
+  })
+
+  return (
+    <animated.div
+      style={{
+        position: 'relative',
+        width: '100%',
+        height: 60,
+        overflow: 'auto',
+        fontSize: '0.5em',
+      }}
+      scrollTop={scroll}>
+      {words.map((word, i) => (
+        <div
+          key={`${word}_${i}`}
+          style={{ width: '100%', height: 50, textAlign: 'center' }}>
+          {word}
+        </div>
+      ))}
+    </animated.div>
+  )
+}
+
+export default Scrolling
