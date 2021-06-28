9c7050597a23f4ea78f272e80fc733e19efda050
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-06-28 11:09:55 -0700

    create on click fade in and out

--- react-animations/src/components/springDemo.tsx
+++ react-animations/src/components/springDemo.tsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import { useSpring, animated } from 'react-spring';
 
 function SpringDemo() {
-  const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } });
+  const fadeIn = { to: { opacity: 1 }, from: { opacity: 0 } }
+  const fadeOut = { to: { opacity: 0 }, from: { opacity: 1 } }
+  const [open, toggle] = useState(false);
+  const props = useSpring(open ? fadeIn: fadeOut);
 
-  return <animated.div style={props}>I will fade in</animated.div>
 
-}
+  return (
+  <div onClick={() => toggle(!open)}>
+    <animated.div style={props}>I will fade in</animated.div> 
+  </div>
+
+  )}
 
 export default SpringDemo
