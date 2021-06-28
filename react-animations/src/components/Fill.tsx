d7c2e070ac7f48be42c8eb1b06ff88b44cb9eed1
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-06-28 10:23:42 -0700

    Update fill animation

--- react-animations/src/components/Fill.tsx
+++ react-animations/src/components/Fill.tsx
@@ -5,19 +5,24 @@ import { useSpring, animated } from "@react-spring/web";
 import styles from '../styles.module.css';
 
 export default function Fill () {
-  const [ref, { width } ] = useMeasure();
+  const [ref, { height, width } ] = useMeasure();
   const [open, toggle] = useState(false);
-  // const actions = {
-  //   LtoR: { width : open ? width : 0 },
-  //   TtoB: { height : open ? height : 0 },
-  // }
-  let props = useSpring({ width : open ? width : 0 });
+  const actions = {
+    LtoR: { width : open ? width : 0 },
+    TtoB: { height : open ? height : 0 },
+  }
+  // const randomProperty = (obj: object) => {
+  //   const values = Object.values(obj)
+  //   return values[Math.floor(Math.random() * values.length)]
+  // };
+  let props = useSpring(randomProperty(actions));
+  
 
   return (
     <div className={styles.container} >
       <div ref={ref} className={styles.main} onClick={() => toggle(!open)}>
-      <animated.div className={styles.fill} style={props} />
-      <animated.div className={styles.content}>{props.width.to(x => x.toFixed(0))}</animated.div>
+      <animated.div className={styles.fill} style={props}/>
+      <animated.div className={styles.content}>{props.width.to((x: number) => x.toFixed(0)) || props.height.to((x: number) => x.toFixed(0))}</animated.div>
       </div>
     </div>
   )
